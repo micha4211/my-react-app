ec98c2c0c2f7369e27558307c27bb6741b2beacc
micha4211 <micha4211@icloud.com>
2023-02-02 21:12:01 -0600

    Lesson 3.2 complete
    Change for importing queryString

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,15 +1,19 @@
 import React, {useState} from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import { CounterButton } from '../CounterButton';
 import { CongratulationsMessage } from '../CongratulationsMessage';
 
 export const CounterButtonPage = () => {
-    const [numberOfClicks, setNumberOfClicks] = useState(0);
+    const location = useLocation();
+    const startingValue = queryString.parse(location.search).startingValue;
+    const [numberOfClicks, setNumberOfClicks] = useState(Number(startingValue));
     const [hideMessage, setHideMessage] = useState(false);
     const increment = () => setNumberOfClicks(numberOfClicks + 1);
 
     return (
         <>
-        <h1>The Counter Button Page</h1>
+        <h1>Counter Button Page</h1>
         {hideMessage
         ? null
         : <CongratulationsMessage 
