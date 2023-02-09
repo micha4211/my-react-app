d64692671129295d3f3438eb296d9047b61291fb
micha4211 <micha4211@icloud.com>
2023-02-08 21:41:47 -0600

    Lesson 4.4 complete
    Use Children props

--- src/CongratulationsMessage.js
+++ src/CongratulationsMessage.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from 'react';
 
-export const CongratulationsMessage = ({numberOfClicks, threshold, onHide}) => {
-   useEffect(() => {
-      return () => console.log('Congratulations Message Unmounting');
-   }, []);
-//the hide button that was set will remove the component for the dom
-   return numberOfClicks >= threshold 
-         ? <>
-            <h1>Congradulations! You've reached {threshold} clicks.</h1>
-            <button onClick={onHide}>Hide</button>
-            </>
-         : null;
+export const CongratulationsMessage = ({threshold, onHide}) => {
 
+//the hide button that was set will remove the component for the dom
+   return (
+      <>
+      <h1>Congradulations! You've reached {threshold} clicks.</h1>
+      <button onClick={onHide}>Hide</button>
+      </>
+   );
 }
--- src/CounterButton.js
+++ src/CounterButton.js
@@ -3,9 +3,9 @@ import React, { useEffect } from 'react';
 export const CounterButton = ({ onIncrement, numberOfClicks }) => {
     useEffect(() => {
         console.log("use effect function called");
-    }, []);
-    //can second arguement that the use effection function would watch for to change.
-    //just leave the array as empty to only have the call run when it first loads.
+    }, [numberOfClicks]);
+    //can pass props as an arguement to run the useEffect function every time.
+    //just leave the array as empty to only
     return (
         <>
         <p>You've clicked the button {numberOfClicks} times</p>
